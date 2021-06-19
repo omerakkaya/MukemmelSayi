030eafebb64dde05b868461b450c7504b60c9d50
omerakkaya <74490637+omerakkaya@users.noreply.github.com>
2021-06-19 17:12:01 +0300

    Add files via upload

---
+++ MukemmelSayilar/MukemmelSayilar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MukemmelSayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int sayilar = 2; sayilar <= 1000; sayilar++)
+            {
+                MukemmelMi(sayilar);
+            }
+            Console.ReadKey();
+        }
+
+        public static void MukemmelMi(int sayi)
+        {
+            int toplamBolen = 0;
+            for(int bolen = 1; bolen < sayi; bolen++)
+            {
+                if (sayi % bolen == 0)
+                    toplamBolen += bolen;
+            }
+
+            if (toplamBolen == sayi)
+                Console.WriteLine(sayi + " sayısı mükemmel sayıdır.");
+        }
+    }
+}
